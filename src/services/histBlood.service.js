43bc3289fdfe46a9b8f7ecab9b447a4485d7cae2
Alisson <llielzinh@gmail.com>
2025-05-06 19:56:23 -0300

    :ok_hand: style: Adding comments

--- src/services/histBlood.service.js
+++ src/services/histBlood.service.js
@@ -44,7 +44,6 @@ const HistBloodService = {
                 data.A_POSITIVO.shortTime = compareShort(data.A_POSITIVO.shortTime, sent);
                 data.A_POSITIVO.longTime = compareLong(data.A_POSITIVO.longTime, sent);
                 return data.A_POSITIVO;
-            break;
             case "a-":
                 data.A_NEGATIVO.received += 1
                 data.A_NEGATIVO.sent = (data.A_NEGATIVO.sent + sent) / data.A_NEGATIVO.received;
@@ -58,42 +57,36 @@ const HistBloodService = {
                 data.B_POSITIVO.shortTime = compareShort(data.B_POSITIVO.shortTime, sent);
                 data.B_POSITIVO.longTime = compareLong(data.B_POSITIVO.longTime, sent);
                 return data.B_POSITIVO;
-            break;
             case "b-":
                 data.B_NEGATIVO.received += 1
                 data.B_NEGATIVO.sent = (data.B_NEGATIVO.sent + sent) / data.B_NEGATIVO.received;
                 data.B_NEGATIVO.shortTime = compareShort(data.B_NEGATIVO.shortTime, sent);
                 data.B_NEGATIVO.longTime = compareLong(data.B_NEGATIVO.longTime, sent);
                 return data.B_NEGATIVO;
-            break;
             case "ab+":
                 data.AB_POSITIVO.received += 1 
                 data.AB_POSITIVO.sent = (data.AB_POSITIVO.sent + sent) / data.AB_POSITIVO.received;
                 data.AB_POSITIVO.shortTime = compareShort(data.AB_POSITIVO.shortTime, sent);
                 data.AB_POSITIVO.longTime = compareLong(data.AB_POSITIVO.longTime, sent);
                 return data.AB_POSITIVO;
-            break;
             case "ab-":
                 data.AB_NEGATIVO.received += 1
                 data.AB_NEGATIVO.sent = (data.AB_NEGATIVO.sent + sent) / data.AB_NEGATIVO.received;
                 data.AB_NEGATIVO.shortTime = compareShort(data.AB_NEGATIVO.shortTime, sent);
                 data.AB_NEGATIVO.longTime = compareLong(data.AB_NEGATIVO.longTime, sent);
-                res.json({message: data.AB_NEGATIVO});
-            break;
+                return data.AB_NEGATIVO;
             case "o+":
                 data.O_POSITIVO.received += 1 
                 data.O_POSITIVO.sent = (data.O_POSITIVO.sent + sent) / data.O_POSITIVO.received;
                 data.O_POSITIVO.shortTime = compareShort(data.O_POSITIVO.shortTime, sent);
                 data.O_POSITIVO.longTime = compareLong(data.O_POSITIVO.longTime, sent);
                 return data.O_POSITIVO;
-            break;
             case "o-":
                 data.O_NEGATIVO.received += 1
                 data.O_NEGATIVO.sent = (data.O_NEGATIVO.sent + sent) / data.O_NEGATIVO.received;
                 data.O_NEGATIVO.shortTime = compareShort(data.O_NEGATIVO.shortTime, sent);
                 data.O_NEGATIVO.longTime = compareLong(data.O_NEGATIVO.longTime, sent);
                 return data.O_NEGATIVO;
-            break;
             default :
                 return 'Informação não encontrada. Tipos sanguíneos para consulta: A+, A-, B+, B-, AB+, AB-, O+ e O-.';
         }
@@ -103,10 +96,12 @@ const HistBloodService = {
 
 // Function called during object update, check this function updates the shortTime.
 function compareShort(stime, atual) {
+    // Returns the result.
     return atual < stime ? atual : stime;
 }
 // Function that checks and updates the longtime, called during object update.
 function compareLong(ltime, atual) {
+    // Returns the result.
     return atual > ltime ? atual : ltime;
 }
 
