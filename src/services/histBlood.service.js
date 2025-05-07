8c9b9f2977e1b0dfc2fbeba20b14aef27c259bd7
Alisson <llielzinh@gmail.com>
2025-05-07 16:37:44 -0300

    :sparkless: feat: Function updateData

--- src/services/histBlood.service.js
+++ src/services/histBlood.service.js
@@ -41,59 +41,35 @@ const HistBloodService = {
         switch (search.toLowerCase()){
             case "a+":
                 securityCopy(data.A_POSITIVO);
-                data.A_POSITIVO.received += 1 
-                data.A_POSITIVO.sent = (data.A_POSITIVO.sent + sent) / data.A_POSITIVO.received;
-                data.A_POSITIVO.shortTime = compareShort(data.A_POSITIVO.shortTime, sent);
-                data.A_POSITIVO.longTime = compareLong(data.A_POSITIVO.longTime, sent);
+                updateData(data.A_POSITIVO, sent);
                 return data.A_POSITIVO;
             case "a-":
                 securityCopy(data.A_NEGATIVO);
-                data.A_NEGATIVO.received += 1
-                data.A_NEGATIVO.sent = (data.A_NEGATIVO.sent + sent) / data.A_NEGATIVO.received;
-                data.A_NEGATIVO.shortTime = compareShort(data.A_NEGATIVO.shortTime, sent);
-                data.A_NEGATIVO.longTime = compareLong(data.A_NEGATIVO.longTime, sent);
+                updateData(data.A_NEGATIVO, sent);
                 return data.A_NEGATIVO;
             case "b+":
                 securityCopy(data.B_POSITIVO);
-                data.B_POSITIVO.received += 1 
-                data.B_POSITIVO.sent = (data.B_POSITIVO.sent + sent) / data.B_POSITIVO.received;
-                data.B_POSITIVO.shortTime = compareShort(data.B_POSITIVO.shortTime, sent);
-                data.B_POSITIVO.longTime = compareLong(data.B_POSITIVO.longTime, sent);
+                updateData(data.B_POSITIVO, sent);
                 return data.B_POSITIVO;
             case "b-":
                 securityCopy(data.B_NEGATIVO);
-                data.B_NEGATIVO.received += 1
-                data.B_NEGATIVO.sent = (data.B_NEGATIVO.sent + sent) / data.B_NEGATIVO.received;
-                data.B_NEGATIVO.shortTime = compareShort(data.B_NEGATIVO.shortTime, sent);
-                data.B_NEGATIVO.longTime = compareLong(data.B_NEGATIVO.longTime, sent);
+                updateData(data.B_NEGATIVO, sent);
                 return data.B_NEGATIVO;
             case "ab+":
                 securityCopy(data.AB_POSITIVO);
-                data.AB_POSITIVO.received += 1 
-                data.AB_POSITIVO.sent = (data.AB_POSITIVO.sent + sent) / data.AB_POSITIVO.received;
-                data.AB_POSITIVO.shortTime = compareShort(data.AB_POSITIVO.shortTime, sent);
-                data.AB_POSITIVO.longTime = compareLong(data.AB_POSITIVO.longTime, sent);
+                updateData(data.AB_POSITIVO, sent);
                 return data.AB_POSITIVO;
             case "ab-":
                 securityCopy(data.AB_NEGATIVO);
-                data.AB_NEGATIVO.received += 1
-                data.AB_NEGATIVO.sent = (data.AB_NEGATIVO.sent + sent) / data.AB_NEGATIVO.received;
-                data.AB_NEGATIVO.shortTime = compareShort(data.AB_NEGATIVO.shortTime, sent);
-                data.AB_NEGATIVO.longTime = compareLong(data.AB_NEGATIVO.longTime, sent);
+                updateData(data.AB_NEGATIVO, sent);
                 return data.AB_NEGATIVO;
             case "o+":
                 securityCopy(data.O_POSITIVO);
-                data.O_POSITIVO.received += 1 
-                data.O_POSITIVO.sent = (data.O_POSITIVO.sent + sent) / data.O_POSITIVO.received;
-                data.O_POSITIVO.shortTime = compareShort(data.O_POSITIVO.shortTime, sent);
-                data.O_POSITIVO.longTime = compareLong(data.O_POSITIVO.longTime, sent);
+                updateData(data.O_POSITIVO, sent);
                 return data.O_POSITIVO;
             case "o-":
                 securityCopy(data.O_NEGATIVO);
-                data.O_NEGATIVO.received += 1
-                data.O_NEGATIVO.sent = (data.O_NEGATIVO.sent + sent) / data.O_NEGATIVO.received;
-                data.O_NEGATIVO.shortTime = compareShort(data.O_NEGATIVO.shortTime, sent);
-                data.O_NEGATIVO.longTime = compareLong(data.O_NEGATIVO.longTime, sent);
+                updateData(data.O_NEGATIVO, sent);
                 return data.O_NEGATIVO;
             default :
                 return 'Informação não encontrada. Tipos sanguíneos para consulta: A+, A-, B+, B-, AB+, AB-, O+ e O-.';
@@ -158,6 +134,15 @@ function compareShort(stime, atual) {
 function compareLong(ltime, atual) {
     // Returns the result.
     return atual > ltime ? atual : ltime;
+}
+
+// Function that updates blood type data.
+function updateData(blood, days){
+    blood.received += 1 
+    blood.sent = (blood.sent + days) / blood.received;
+    blood.shortTime = compareShort(blood.shortTime, days);
+    blood.longTime = compareLong(blood.longTime, days);
+    return blood;
 }
 
 // Function that makes the backup during the update.
