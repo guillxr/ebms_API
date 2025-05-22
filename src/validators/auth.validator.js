91f26d8d057812b7ee50f36c6df2f0dc7aa4ba7d
Francisco Gustavo Souza Oliveira <136463238+guillxr@users.noreply.github.com>
2025-05-21 22:31:26 -0300

    ♻️ refactor: update auth validator for new user creation

--- src/validators/auth.validator.js
+++ src/validators/auth.validator.js
@@ -1,28 +1,30 @@
-const { body } = require('express-validator');
+import { body } from 'express-validator';
 
 const loginValidation = [
-  body('username')
-    .notEmpty()
-    .withMessage('Username is required')
-    .isString()
-    .withMessage('Username must be a string'),
+  body('email')
+    .notEmpty().withMessage('Email is required')
+    .isEmail().withMessage('Invalid email'),
 
   body('password')
-    .notEmpty()
-    .withMessage('Password is required')
-    .isLength({ min: 6 })
-    .withMessage('Password must be at least 6 characters'),
+    .notEmpty().withMessage('Password is required')
+    .isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
 ];
 
-const createAdminValidation = [
-  body('credentials')
-    .notEmpty()
-    .withMessage('Credentials is required')
-    .equals(process.env.CREDENTIALS)
-    .withMessage('Access credentials denied!'),
+const createUserValidation = [
+  body('email')
+    .notEmpty().withMessage('Email is required')
+    .isEmail().withMessage('Invalid email'),
+
+  body('password')
+    .notEmpty().withMessage('Password is required')
+    .isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
+
+  body('role')
+    .notEmpty().withMessage('Role is required')
+    .isIn(['DONOR', 'ADMIN']).withMessage('Role must be DONOR or ADMIN'),
 ];
 
 module.exports = {
   loginValidation,
-  createAdminValidation,
+  createUserValidation,
 };
