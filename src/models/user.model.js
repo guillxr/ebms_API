959c6774e8f94a2b5f589325e987f611d272f49d
Francisco Gustavo Souza Oliveira <136463238+guillxr@users.noreply.github.com>
2025-05-21 22:30:12 -0300

    🆕 feat: add user model with role and relations

---
+++ src/models/user.model.js
@@ -0,0 +1,87 @@
+import log from '@utils/logger.js';
+
+import { prisma } from '../prisma/client.js';
+
+/**
+ * @module UserModel
+ */
+const UserModel = {
+  /**
+   * Finds a user by their email.
+   *
+   * @async
+   * @function findByEmail
+   * @param {string} email
+   * @returns {Promise<Object|null>}
+   */
+  findByEmail: async (email) => {
+    try {
+      return await prisma.user.findUnique({
+        where: { email },
+        include: { donor: true, admin: true },
+      });
+    } catch (error) {
+      log(`Error finding user by email: ${error.message}`, 'error');
+      throw error;
+    }
+  },
+
+  /**
+   * Finds a user by their ID.
+   *
+   * @async
+   * @function findById
+   * @param {string} id
+   * @returns {Promise<Object|null>}
+   */
+  findById: async (id) => {
+    try {
+      return await prisma.user.findUnique({
+        where: { id },
+        include: { donor: true, admin: true },
+      });
+    } catch (error) {
+      log(`Error finding user by ID: ${error.message}`, 'error');
+      throw error;
+    }
+  },
+
+  /**
+   * Deletes a user by ID.
+   *
+   * @async
+   * @function deleteUser
+   * @param {string} id
+   * @returns {Promise<Object>}
+   */
+  deleteUser: async (id) => {
+    try {
+      return await prisma.user.delete({
+        where: { id },
+      });
+    } catch (error) {
+      log(`Error deleting user: ${error.message}`, 'error');
+      throw error;
+    }
+  },
+
+  /**
+   * Retrieves all users with their roles.
+   *
+   * @async
+   * @function findAll
+   * @returns {Promise<Array>}
+   */
+  findAll: async () => {
+    try {
+      return await prisma.user.findMany({
+        include: { donor: true, admin: true },
+      });
+    } catch (error) {
+      log(`Error retrieving users: ${error.message}`, 'error');
+      throw error;
+    }
+  },
+};
+
+export default UserModel;
